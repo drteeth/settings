a2448e2ee8d17b3d8bc12e5450f5732d40f15e97
Benjamin Moss <drteeth@gmail.com>
2016-03-10 23:20:23 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -snf settings/.ackrc
+ln -snf settings/.agignore
+ln -snf settings/.gemrc
+ln -snf settings/.gitconfig
+ln -snf settings/.gitignore_global
+ln -snf settings/.tmux.conf
+ln -snf settings/.vimrc
