4bb128fcc809571cd6b3ab9369e77a41150fb184
Benjamin Moss <drteeth@gmail.com>
2016-05-07 10:56:51 -0400

    install script now run from the project root instead of from ~

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-ln -snf settings/.ackrc
-ln -snf settings/.agignore
-ln -snf settings/.gemrc
-ln -snf settings/.gitconfig
-ln -snf settings/.gitignore_global
-ln -snf settings/.tmux.conf
-ln -snf settings/.vimrc
-ln -snf settings/.spacemacs
+ln -snf `pwd`/.ackrc ~/.ackrc
+ln -snf `pwd`/.agignore ~/.agignore
+ln -snf `pwd`/.gemrc ~/.gemrc
+ln -snf `pwd`/.gitconfig ~/.gitconfig
+ln -snf `pwd`/.gitignore_global ~/.gitignore_global
+ln -snf `pwd`/.tmux.conf ~/.tmux.conf
+ln -snf `pwd`/.vimrc ~/.vimrc
+ln -snf `pwd`/.spacemacs ~/.spacemacs
+
+# neovim
+mkdir -p ~/.config/nvim
+ln -snf `pwd`/nvim-init.vim ~/.config/nvim
+
+# vim-plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
